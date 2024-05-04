7ae427b62895dffb4d03cd600dab5fdf4678ab97
LunexCoding <lunexcoding@vk.com>
2024-05-04 12:44:29 +0700

    Caching received data

--- dataStructures/referenceBook.py
+++ dataStructures/referenceBook.py
@@ -7,25 +7,36 @@ from tools.tables import DatabaseTables
 class _ReferenceBook:
     def __init__(self, table):
         self._table = table
+        self._rows = []
 
     def loadRows(self):
         result = g_commandCenter.execute(Constants.LOAD_COMMAND.format(self._table))
         if result["Status"] == COMMAND_STATUS.EXECUTED:
-            return result["Result"]
-        return None
+            if result["Result"] is not None:
+                for row in result["Result"]:
+                    self._rows.append(row)
+                return result["Result"]
+            return None
 
     def insertRow(self, data):
+        del data["ID"]
+        del data["ContractNumber"]
         columns = "[*]"
         values = ",".join(map(str, list(data.values())))
         command = Constants.ADD_COMMAND.format(self._table, columns, [values]).replace("'", "")
         result = g_commandCenter.execute(command)
         if result["Status"] == COMMAND_STATUS.EXECUTED:
+            self._rows.append(result["Result"])
             return result["Result"]
         return None
 
     @property
     def table(self):
         return self._table
+
+    @property
+    def rows(self):
+        return self._rows
 
 
 g_bookIncomingDocuments = _ReferenceBook(DatabaseTables.INCOMING_DOCUMENTS)
